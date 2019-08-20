284f0d10ba4848d2638f2be10cf30af9f43b9342
Taylor Wu <taylorwu21@gmail.com>
2019-08-20 09:17:53 -0600

    WIP

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React from 'react';
-import { EditorState } from "prosemirror-state"
+import {
+  EditorState,
+  NodeSelection,
+  Selection,
+  TextSelection,
+  SelectionRange
+} from "prosemirror-state"
 import {EditorView} from "prosemirror-view"
 import {Schema, DOMParser} from "prosemirror-model"
 import {schema} from "prosemirror-schema-basic"
 import {dropCursor} from "prosemirror-dropcursor"
 import {gapCursor} from "prosemirror-gapcursor"
+import {
+  InputRule,
+  inputRules,
+} from 'prosemirror-inputrules';
 import {keymap} from "prosemirror-keymap"
 import {history} from "prosemirror-history"
 import {baseKeymap} from "prosemirror-commands"
 import { buildKeymap } from './keymaps';
 import _ from 'lodash';
 
+import './app.css';
+
 const pDOM = ["p", 0];
 const brDOM = ["br"];
-const spanDOM = ["span"];
+const spanDOM = ["span", 0];
+const strongDOM = ["strong", 0];
 
 const nodes = {
   // :: NodeSpec The top level document node.
@@ -46,41 +59,86 @@ const nodes = {
 }
 
 const marks = {
-  blueVariable: {
+  knownVariable: {
     inclusive: false,
     parseDOM: [
       {tag: "span"},
-      {style: "color: blue;"}
     ],
-    toDOM() { return spanDOM }
+    toDOM: (node) => [
+      'span',
+      {
+        class: "knownVariable"
+      }
+    ]
   },
 
-  redVariable: {
+  unknownVariable: {
     inclusive: false,
     parseDOM: [
       {tag: "span"},
       {style: "color: red;"}
     ],
-    toDOM() { return spanDOM }
+    toDOM: (node) => [
+      'span',
+      {
+        class: "unknownVariable"
+      }
+    ]
   },
 
-  grayVariable: {
+  backendVariable: {
     inclusive: false,
     parseDOM: [
       {tag: "span"},
       {style: "color: gray;"}
     ],
-    toDOM() { return spanDOM }
-  }
+    toDOM: (node) => [
+      'span',
+      {
+        class: "backendVariable"
+      }
+    ]
+  },
+
+  strong: {
+    parseDOM: [{tag: "strong"},
+               // This works around a Google Docs misbehavior where
+               // pasted content will be inexplicably wrapped in `<b>`
+               // tags with a font-weight normal.
+               {tag: "b", getAttrs: node => node.style.fontWeight != "normal" && null},
+               {style: "font-weight", getAttrs: value => /^(bold(er)?|[5-9]\d{2,})$/.test(value) && null}],
+    toDOM() { return strongDOM }
+  },
 }
 
 const plugins = (options) => {
+  const variableRule = new InputRule(
+    /:(\w+):/,
+    (state, match, start, end) => {
+      const transaction = state.tr;
+      const mark = state.config.schema.marks.knownVariable.instance;
+      const { $anchor, $head } = transaction.selection;
+      const selection = TextSelection.between($anchor, $head);
+      console.log(selection, 'selection');
+
+      transaction.addStoredMark(mark);
+      const newState = state.apply(transaction);
+      window.view.updateState(newState);
+      const $from = state.selection.$anchor;
+
+      const node = NodeSelection.create(state.doc, start);
+    }
+  )
+
+  const inputRulesPlugin = inputRules({ rules: [variableRule] });
+
   let plugins = [
     keymap(buildKeymap(options.schema, options.mapKeys)),
     keymap(baseKeymap),
     dropCursor(),
     gapCursor(),
     history(),
+    inputRulesPlugin,
   ]
 
   return plugins;
@@ -98,27 +156,17 @@ class App extends React.Component {
       marks,
     });
 
-    console.log(mySchema, 'mySchema');
+    // console.log(mySchema, 'mySchema');
 
     window.view = new EditorView(document.querySelector('#editor'), {
       state: EditorState.create({
         schema: mySchema,
-        // doc: DOMParser.fromSchema(mySchema).parse(document.querySelector('#content')),
+        doc: DOMParser.fromSchema(mySchema).parse(document.querySelector('#content')),
         plugins: plugins({ schema: mySchema }),
       }),
-      // dispatchTransaction: (transaction) => {
-      //   console.log(window.view)
-      //   console.log(transaction);
-
-      //   window.view.state.applyTransaction(transaction);
-
-      //   // window.view.applyTransaction(transaction);
-      //   // window.view.updateState(transaction);
-
-      // }
     })
 
-    console.log(window.view);
+    console.log(window.view, 'view');
   }
 
   render() {
